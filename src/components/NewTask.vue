ea068f048e6fdb10bff9dc113704f2394f72e423
Cristina <krishee107@gmail.com>
2022-10-19 18:29:14 +0200

    WIP: Formulario de crear tarea

--- src/components/NewTask.vue
+++ src/components/NewTask.vue
@@ -1,12 +1,18 @@
 <template>
     <div class="section">
         <div class="container">
-            <form @submit.prevent="">
+            <form @submit.prevent="onSubmit">
                 <div class="field">
                     <div class="control is-large is-loading">
-                        <input class="input is-large" type="text" placeholder="Introduce la nueva tarea...">
+                        <input class="input is-large" v-model="title" type="text" placeholder="Introduce el TITULO de la nueva tarea..." required>
                     </div>
                 </div>
+                <div class="field" v-if="title.length > 0">
+                    <div class="control is-large">
+                        <input class="input is-large" v-model="description" type="text" placeholder="Introduce la  descripción de la nueva tarea...">
+                    </div>
+                </div>
+
             </form>
         </div>
     </div>
@@ -14,7 +20,14 @@
 </template>
 
 <script setup>
+import {ref} from 'vue'
 
+const title = ref('');
+const description = ref();
+
+const onSubmit = ()=>{
+    console.log(title, description)
+}
 </script>
 
 <style scoped>
