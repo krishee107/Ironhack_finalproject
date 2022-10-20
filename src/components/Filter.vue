0b15be96d9eac05f59222cbd44ed6d0a5585a967
Cristina <krishee107@gmail.com>
2022-10-20 18:10:55 +0200

    Style: Estilo básico para los botones de filtro

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -2,9 +2,9 @@
     <div class="section">
         <div class="container">
             <div class="buttons is-justify-content-center is-flex">
-                <button @click="changeFilter('all')" class=" button">All</button>
-                <button @click="changeFilter('activa')" class=" button">Todo</button>
-                <button @click="changeFilter('completada')" class="button">Done</button>
+                <button @click="changeFilter('all')" class=" button is-normal">All</button>
+                <button @click="changeFilter('activa')" class=" button is-normal">Todo</button>
+                <button @click="changeFilter('completada')" class="button is-normal">Done</button>
             </div>
         </div>
     </div>
@@ -22,4 +22,8 @@ const changeFilter = (filter) =>{
 </script>
 
 <style scoped>
+button{
+    min-width: 100px;
+    box-shadow: 0 0.5em 1em -0.125em rgb(10 10 10 / 20%), 0 0 0 1px rgb(10 10 10 / 2%);
+}
 </style>
